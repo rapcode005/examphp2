8c787ca3aaaebeb2a7798223bbbce75e4a00f7be
rapcode005 <32093759+rapcode005@users.noreply.github.com>
2020-12-01 15:13:42 +0800

    Update headerhomeadmin.php

--- headerhomeadmin.php
+++ headerhomeadmin.php
@@ -9,5 +9,15 @@
 		<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		</script>
+		<script>
+			function validateForm() {
+			  var x = document.forms["myform"]["username"].value;
+			  var y = document.forms["myform"]["password"].value;
+				  if (x == "" || y == ") {
+				    alert("Username/Password must be filled out");
+				    return false;
+			  }
+			}
+		</script>
 	</head>
 <body>
